2a924e875bd4b70a4a7758f0dee649d67c36b474
dinnyyy <160369794+dinnyyy@users.noreply.github.com>
2026-04-11 11:58:03 +1000

    Changed title to StdOut

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ function Navbar() {
 
   return (
     <header className="navbar">
-      <div className="brand">AI Interview Sim</div>
+      <div className="brand">StdOut Interview Simulator</div>
       <nav className="nav-links">
         <Link to="/interview/setup">New Interview</Link>
         <Link to="/interviews/old">Previous Interviews</Link>
