7bae59775c5178eac251fbf886c6b0d72dc67253
jeremyylimmm <jeremylim.0304@gmail.com>
2026-04-12 01:23:34 +1000

    Fix ts

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import API_BASE_URL from "../config/api";
 
 // https://vite.dev/config/
 export default defineConfig({
